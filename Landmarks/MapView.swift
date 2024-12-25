71d25921a5289e960c5fca01597905e0c1823ac0
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2024-12-25 21:12:54 +0100

    Create MapView with Himeji Castle coords

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,26 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Serrano Soria, Juan on 25/12/2024.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map(initialPosition: .region(region))
+    }
+    
+    private var region: MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 34.839722, longitude: 134.693889),
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    }
+}
+
+#Preview {
+    MapView()
+}

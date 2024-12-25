bfc6adc306b35f0b5ea932e2b0cd5d04ed03c8cd
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2024-12-25 20:32:26 +0100

    Initial commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Serrano Soria, Juan on 25/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

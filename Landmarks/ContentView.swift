d3314934fdfc6e1120e6b204370ca30f22d8e3fa
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2024-12-25 21:14:36 +0100

    Added basic view of landmark of Himeji Castle

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,12 +10,29 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            MapView()
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("姫路城")
+                    .font(.title)
+                HStack {
+                    Text("兵庫県姫路市")
+                    Spacer()
+                    Text("関西")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                Divider()
+                Text("姫路城について")
+                    .font(.title2)
+                Text("姫路城（ひめじじょう）は、兵庫県姫路市にある日本の城。江戸時代初期に建てられた天守や櫓等の主要建築物が現存し、国宝や重要文化財に指定されている。")
+            }
+            .padding()
+            Spacer() // to move all the content to the top
         }
-        .padding()
     }
 }
 

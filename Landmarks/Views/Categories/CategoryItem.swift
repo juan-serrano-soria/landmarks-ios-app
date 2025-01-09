03fa59d9b55709fd78d717ed72e10cde82784c4e
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2025-01-09 14:29:14 +0100

    Added category row and item

---
+++ Landmarks/Views/Categories/CategoryItem.swift
@@ -0,0 +1,29 @@
+//
+//  CategoryItem.swift
+//  Landmarks
+//
+//  Created by Serrano Soria, Juan on 09/01/2025.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var landmark: Landmark
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 155, height: 155)
+                    .cornerRadius(5)
+            Text(landmark.name)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+#Preview {
+    CategoryItem(landmark: ModelData().landmarks[0])
+}
